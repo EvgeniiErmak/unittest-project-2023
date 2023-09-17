718d2fc78065a1aad3126c897289cae14b71959d
EvgeniiErmak <djermak3000@mail.ru>
2023-09-17 10:00:57 +0300

    unittest upgrade

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -5,9 +5,16 @@ from utils import arrs
 class TestArrs(unittest.TestCase):
 
     def test_get(self):
-        self.assertEqual(arrs.get([1, 2, 3], 1, 3), 2)
-        self.assertEqual(arrs.get([], 0, 0), 0)
+        self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 2)
+        self.assertEqual(arrs.get([], 0, "test"), "test")
 
     def test_slice(self):
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
         self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+        self.assertIsNot(test_array := [1, 2, 3], arrs.my_slice(test_array))
+        self.assertIsInstance(arrs.my_slice([]), list)
+        self.assertEqual(arrs.my_slice([1, 2, 3], -100), [1, 2, 3])
+        self.assertEqual(arrs.my_slice([]), [])
+        self.assertEqual(arrs.my_slice([], 1), [])
+        self.assertEqual(arrs.my_slice([], 1, 3), [])
+        self.assertEqual(arrs.my_slice(test_array := [1, 2, 3], -len(test_array) + 1), [2, 3])
